e6c43721cda21661c7f4049dea1e1616fd0e45c9
drussell9 <drussell9@gatech.edu>
2015-07-27 14:26:13 -0400

    LCD TEST
    LCD Hello world for PIC  16f1936 microchip microcontroller

---
+++ LCD HelloWorld.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   LCD HelloWorld.c
+ * Author: DDR02
+ *
+ * Created on July 27, 2015, 8:55 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ build/default/production/LCD HelloWorld.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/LCD\ HelloWorld.d  \
+ build/default/production/LCD\ HelloWorld.p1:  \
+ LCD\ HelloWorld.c 
